64350c30c54f14565951ab01732903fdf3720e36
arfigures <62619080+arfigures@users.noreply.github.com>
2022-06-04 16:28:46 -0400

    pushnoe

--- src/App.jsx
+++ src/App.jsx
@@ -31,11 +31,10 @@ const App = () => {
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery}/>
-      
+      <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
     </div>
   );
 };
-// <Testimonials data={landingPageData.Testimonials} />
 export default App;
